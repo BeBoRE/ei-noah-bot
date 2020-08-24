a01bcc11e78fd9675c0e10ab65105b11394ef151
Ayrton-Taede Tromp <ayrton.taede@gmail.com>
2020-08-24 20:13:35 +0200

    Added ability to add and remove users from lobby

--- src/routes/LobbyRouter.ts
+++ src/routes/LobbyRouter.ts
@@ -10,6 +10,7 @@ import {
   DiscordAPIError,
 } from 'discord.js';
 import { getRepository } from 'typeorm';
+import { saveUserData } from '../data';
 import { GuildUser } from '../entity/GuildUser';
 import { TempChannel } from '../entity/TempChannel';
 import Router from '../Router';
@@ -65,7 +66,7 @@ async function activeTempChannel(guildUser : GuildUser, client : Client) : Promi
   if (!guildUser.tempChannel) return undefined;
 
   try {
-    const activeChannel = await client.channels.fetch(guildUser.tempChannel.id, false);
+    const activeChannel = await client.channels.fetch((await guildUser.tempChannel).id, false);
     if (activeChannel instanceof VoiceChannel) {
       return activeChannel;
     }
@@ -95,7 +96,7 @@ createRouter.use(DiscordUser, async ({
     const activeChannel = await activeTempChannel(guildUser, msg.client);
 
     if (activeChannel) {
-      msg.channel.send('You already have a lobby');
+      msg.channel.send('Je hebt al een lobby');
     } else {
       const createdChannel = await createTempChannel(msg.guild, msg.channel.parentID, users, msg.author, msg.client.user, { muted: flags.some((a) => a === 'nospeak') });
 
@@ -106,6 +107,7 @@ createRouter.use(DiscordUser, async ({
       const tempChannel = tempRep.create({ guildUser, id: createdChannel.id });
 
       try {
+        await saveUserData(guildUser);
         await tempRep.save(tempChannel);
       } catch (err) {
         createdChannel.delete();
@@ -113,14 +115,120 @@ createRouter.use(DiscordUser, async ({
       }
     }
   }
-
-  return null;
 });
 
 router.use('create', createRouter);
 
-router.use('remove', (info) => {
-  info.msg.channel.send('Removing channel');
+router.use('add', async ({ params, msg, guildUser }) => {
+  if (msg.channel instanceof DMChannel) {
+    msg.channel.send('Dit commando kan alleen gebruikt worden op een server');
+    return;
+  }
+
+  const nonUsers = params.filter((param) => !(param instanceof DiscordUser));
+  const users = params.filter((param): param is DiscordUser => param instanceof DiscordUser);
+
+  if (nonUsers.length > 0) {
+    msg.channel.send('Alleen user mention(s) mogelijk als argument');
+    return;
+  }
+
+  const activeChannel = await activeTempChannel(guildUser, msg.client);
+
+  if (!activeChannel) {
+    msg.channel.send('Je hebt nog geen lobby aangemaakt\nMaak één aan met `ei lobby create`');
+    return;
+  }
+
+  if (activeChannel.parentID !== msg.channel.parentID) {
+    msg.channel.send('Je lobby is aanwezig in een andere categorie dan deze');
+    return;
+  }
+
+  const allowedUsers : DiscordUser[] = [];
+  const alreadyAllowedUsers : DiscordUser[] = [];
+
+  users.forEach((user) => {
+    if (activeChannel.permissionOverwrites.some((o) => user.id === o.id)) {
+      alreadyAllowedUsers.push(user);
+    } else {
+      activeChannel.updateOverwrite(user, {
+        CONNECT: true,
+        SPEAK: true,
+      });
+
+      allowedUsers.push(user);
+    }
+  });
+
+  let allowedUsersMessage : string;
+  if (!allowedUsers.length) allowedUsersMessage = 'Geen user(s) toegevoegd';
+  else allowedUsersMessage = `${allowedUsers.map((user) => user.username).join(', ')} ${allowedUsers.length > 1 ? 'mogen' : 'mag'} nu naar binnen`;
+
+  let alreadyInMessage : string;
+  if (!alreadyAllowedUsers.length) alreadyInMessage = '';
+  else alreadyInMessage = `${alreadyAllowedUsers.map((user) => user.username).join(', ')} ${alreadyAllowedUsers.length > 1 ? 'konden' : 'kon'} al naar binnen`;
+
+  msg.channel.send(`${allowedUsersMessage}\n${alreadyInMessage}`);
+});
+
+router.use('remove', async ({ params, msg, guildUser }) => {
+  if (msg.channel instanceof DMChannel) {
+    msg.channel.send('Dit commando kan alleen gebruikt worden op een server');
+    return;
+  }
+
+  const nonUsers = params.filter((param) => !(param instanceof DiscordUser));
+  const users = params.filter((param): param is DiscordUser => param instanceof DiscordUser);
+
+  if (nonUsers.length > 0) {
+    msg.channel.send('Alleen user mention(s) mogelijk als argument');
+    return;
+  }
+
+  const activeChannel = await activeTempChannel(guildUser, msg.client);
+
+  if (!activeChannel) {
+    msg.channel.send('Je hebt nog geen lobby aangemaakt\nMaak één aan met `ei lobby create`');
+    return;
+  }
+
+  if (activeChannel.parentID !== msg.channel.parentID) {
+    msg.channel.send('Je lobby is aanwezig in een andere categorie dan deze');
+    return;
+  }
+
+  let triedRemoveSelf = false;
+  let triedRemoveEi = false;
+  const notRemoved = users.filter((user) => {
+    let removed = false;
+    if (user.id === msg.author.id) triedRemoveSelf = true;
+    else if (user.id === msg.client.user.id) triedRemoveEi = true;
+    else {
+      const member = activeChannel.members.get(user.id);
+      if (member && member.voice.channelID === activeChannel.id) {
+        member.voice.setChannel(null);
+        removed = true;
+      }
+
+      if (activeChannel.permissionOverwrites.has(user.id)) {
+        activeChannel.permissionOverwrites.get(user.id).delete();
+        removed = true;
+      }
+    }
+
+    return !removed;
+  });
+
+  if (notRemoved.length > 0) {
+    let message = `${notRemoved.map((user) => user.username).join(', ')} ${notRemoved.length > 1 ? 'zijn' : 'is'} niet verwijderd`;
+    if (triedRemoveSelf) message += '\nJe kan jezelf niet verwijderen';
+    if (triedRemoveEi) message += '\nEi Noah is omnipresent';
+
+    msg.channel.send(message);
+  } else {
+    msg.channel.send('Alle gegeven personen zijn verwijderd');
+  }
 });
 
 export default router;
