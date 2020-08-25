b2840d503ebe8e773012147b363d5c5b32c3c789
Hazent <vinny.boy@live.nl>
2020-08-26 01:31:30 +0200

    Merge commit

--- src/routes/Birthday.ts
+++ src/routes/Birthday.ts
@@ -1,18 +1,24 @@
 import Router from '../Router';
+
 const Discord = require('discord.js');
 
 const router = new Router();
 
-router.use('set', async ({ msg, guildUser}) => 
-{
-    msg.channel.send('Je verjaardag kan ook later toegevoegd worden.');
+router.use('set', async ({ msg, guildUser }) => {
+  msg.channel.send('Je verjaardag kan ook later toegevoegd worden.');
 });
 
-router.use('give', async ({ msg, guildUser}) =>
-{
-    //Toevoeging van een role op basis van ID
-    msg.member.roles.add('744926444756533358');
-    msg.channel.send('You have gained a birthday.');
+router.use('give', async ({ msg, guildUser }) => {
+  // Toevoeging van een role op basis van ID
+  const role = msg.member.roles.add('744926444756533358');
+  const sendMessage = msg.channel.send('You have gained a birthday.');
+
+  await role;
+  await sendMessage;
+});
+
+router.use('help', async ({ msg, guildUser }) => {
+  msg.channel.send('Hulp bericht is onderweg');
 });
 
 export default router;
