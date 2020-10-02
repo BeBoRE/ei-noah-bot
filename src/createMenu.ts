8f221305b1424837d2a40c6620289440812c8e82
Ayrton-Taede BeFoRe <ayrton.taede@gmail.com>
2020-10-02 19:34:01 +0200

    Meer Knopjes
    *Veranderd:* Menu's laten nu 5 items zien (was 3)

--- src/createMenu.ts
+++ src/createMenu.ts
@@ -16,7 +16,7 @@ async function createMenu<T>(
   ...extraButtons : ExtraButton[]
 ) {
   const emotes = [
-    '1️⃣', '2️⃣', '3️⃣',
+    '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣',
   ];
   const pageLeft = '◀️';
   const pageRight = '▶️';
