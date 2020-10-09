0cec2776be914bdff1f0d3e900a254488378073e
Ayrton-Taede Tromp <ayrton.taede@gmail.com>
2020-10-09 14:08:28 +0200

    Added ability to add remove regions from rapport

--- src/routes/CoronaRouter.ts
+++ src/routes/CoronaRouter.ts
@@ -2,8 +2,7 @@ import fetch from 'node-fetch';
 import moment from 'moment';
 import { CronJob } from 'cron';
 import UserCoronaRegions from '../entity/UserCoronaRegions';
-import { User } from '../entity/User';
-import Router from '../Router';
+import Router, { Handler } from '../Router';
 import CoronaData, { CoronaInfo } from '../entity/CoronaData';
 
 const router = new Router();
@@ -11,6 +10,70 @@ const router = new Router();
 router.use(null, async ({ msg }) => {
   msg.channel.send('a');
 });
+
+const addHandler : Handler = async ({
+  msg, user, params, em,
+}) => {
+  if (!params.every((param) : param is string => typeof param === 'string')) {
+    msg.channel.send('Jij denkt dat een persoon een regio is?');
+    return;
+  }
+
+  const region = params.filter((param) : param is string => typeof param === 'string').join(' ');
+  await user.coronaRegions.init();
+  const currentRegions = user.coronaRegions.getItems()
+    .find((r) => r.region.toLowerCase() === region.toLowerCase());
+
+  if (currentRegions) {
+    msg.channel.send('Deze regio had je al toegevoegd');
+    return;
+  }
+
+  const coronaReport = (await em.getRepository(CoronaData)
+    .findAll({ limit: 500 }))
+    .find((cr) => cr.community.toLowerCase() === region.toLowerCase());
+  if (!coronaReport) {
+    msg.channel.send(`${region} is niet een regio in de database`);
+    return;
+  }
+
+  const newRegion = new UserCoronaRegions();
+
+  newRegion.region = coronaReport.community;
+  newRegion.user = user;
+
+  em.persist(newRegion);
+
+  msg.channel.send(`${coronaReport.community} toegevoegd aan je dagelijkse raport`);
+};
+
+router.use('add', addHandler);
+router.use('toevoegen', addHandler);
+
+const removeHandler : Handler = async ({ msg, user, params }) => {
+  if (!params.every((param) : param is string => typeof param === 'string')) {
+    msg.channel.send('Jij denkt dat een persoon een regio is?');
+    return;
+  }
+
+  const region = params.filter((param) : param is string => typeof param === 'string').join(' ');
+  await user.coronaRegions.init();
+  const dbRegion = user.coronaRegions.getItems()
+    .find((r) => r.region.toLowerCase() === region.toLowerCase());
+
+  if (!dbRegion) {
+    msg.channel.send('Je hebt deze regio niet toegevoegd');
+    return;
+  }
+
+  user.coronaRegions.remove(dbRegion);
+
+  msg.channel.send(`${dbRegion.region} verwijderd van je dagelijkse raport`);
+};
+
+router.use('remove', removeHandler);
+router.use('verwijder', removeHandler);
+router.use('delete', removeHandler);
 
 router.onInit = async (client, orm) => {
   const refreshData = async () => {
