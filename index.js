32c6e850d5923cd6df45e1e20e419221bd256460
Ayrton-Taede BeFoRe <ayrton.taede@gmail.com>
2020-10-19 00:57:54 +0200

    set target to es5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 const next = require('next')
 const fs = require('fs')
 
-const app = next({dev: false})
+const app = next({dev: true})
 const handler = app.getRequestHandler()
 
 const server = http.createServer(handler);
