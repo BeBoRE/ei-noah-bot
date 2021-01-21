5f3f24f44d02731bef50dd6f84e5d82f4a1a7d07
Ayrton-Taede BeFoRe <ayrton.taede@gmail.com>
2021-01-21 22:03:33 +0100

    bday set now checks range

--- src/routes/Birthday.ts
+++ src/routes/Birthday.ts
@@ -26,6 +26,14 @@ router.use('set', async ({ user, params }) => {
   const birth = new Date(parseInt(args[2], 10), parseInt(args[1], 10) - 1, parseInt(args[0], 10));
   const birth1 = moment(birth);
 
+  if (birth1.isAfter(new Date())) {
+    return 'Je geboorte kan niet in de toekomst zijn';
+  }
+
+  if (moment().subtract(80, 'years').isAfter(birth1)) {
+    return 'Zo oud? 🤔';
+  }
+
   // eslint-disable-next-line no-param-reassign
   user.birthday = birth1.toDate();
 
