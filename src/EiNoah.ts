d4970f9481f3de87c029a2d3a4572cf7491772e9
Ayrton-Taede BeFoRe <ayrton.taede@gmail.com>
2021-02-16 23:46:23 +0100

    Improved error handling

--- src/EiNoah.ts
+++ src/EiNoah.ts
@@ -1,16 +1,26 @@
 import {
-  Client, User as DiscordUser, TextChannel, NewsChannel, Role,
+  Client, User as DiscordUser, TextChannel, NewsChannel, Role, Permissions,
 } from 'discord.js';
 import { Connection, IDatabaseDriver, MikroORM } from '@mikro-orm/core';
 import Router, { Handler, messageParser } from './Router';
 
-const errorToChannel = async (channelId : string, client : Client, err : Error) => {
+enum ErrorType {
+  Uncaught,
+  Unhandled,
+}
+
+const errorToChannel = async (channelId : string, client : Client, err : Error, type?: ErrorType) => {
   const errorChannel = await client.channels.fetch(channelId);
   if (errorChannel instanceof TextChannel
      || errorChannel instanceof NewsChannel
   ) {
-    errorChannel.send(`**${err?.name}**\n\`\`\`${err?.stack}\`\`\``, { split: true });
+    let header = '';
+    if (type === ErrorType.Uncaught) header = '**Uncaught**';
+    if (type === ErrorType.Unhandled) header = '**Unhandled**';
+    return errorChannel.send(`${header}\n**${err?.name}**\n\`\`\`${err?.stack}\`\`\``, { split: true });
   }
+
+  return null;
 };
 
 class EiNoah {
@@ -46,7 +56,7 @@ class EiNoah {
       console.log('client online');
     });
 
-    this.client.on('message', async (msg) => {
+    this.client.on('message', (msg) => {
       if (msg.author !== this.client.user && msg.content) {
         const splitted = msg.content.split(' ').filter((param) => param);
 
@@ -54,47 +64,44 @@ class EiNoah {
         const botMention = `<@${this.client.user?.id}>`;
         const botNickMention = `<@!${this.client.user?.id}>`;
 
-        if (splitted[0] === botMention || splitted[0].toUpperCase() === 'EI' || splitted[0] === botNickMention) {
-          msg.channel.startTyping();
+        let canSendMessage = true;
+
+        if ((msg.channel instanceof TextChannel || msg.channel instanceof NewsChannel) && msg.client.user) {
+          if (!msg.channel.permissionsFor(msg.client.user)?.has(Permissions.FLAGS.SEND_MESSAGES)) canSendMessage = false;
+        }
+
+        if ((splitted[0] === botMention || splitted[0].toUpperCase() === 'EI' || splitted[0] === botNickMention) && canSendMessage) {
+          msg.channel.startTyping(10000).catch(() => { });
           const em = orm.em.fork();
 
-          messageParser(msg, em).then((info) => {
-            this.router.handle(info)
-              .then(async (response) => {
-                if (response) {
-                  if (typeof (response) !== 'string') {
-                    await msg.channel.send(response);
-                  } else {
-                    await msg.channel.send(response, { split: true });
-                  }
+          messageParser(msg, em)
+            .then((info) => this.router.handle(info))
+            .then((response) => {
+              if (response) {
+                if (typeof (response) !== 'string') {
+                  return msg.channel.send(response).catch(() => { });
                 }
-                await em.flush();
-              })
-              .catch(async (err : Error) => {
-                if (process.env.NODE_ENV !== 'production') {
-                // Error message in development
-                  errorToChannel(msg.channel.id, msg.client, err);
-                } else {
-                // Error message in productie
-                  msg.channel.send('Je sloopt de hele boel hier!\nGeen idee wat ik hiermee moet doen D:');
-                  if (process.env.ERROR_CHANNEL) {
-                    errorToChannel(process.env.ERROR_CHANNEL, msg.client, err);
-                  }
-                }
-              });
-          }).catch((err) => {
-            // Dit wordt gecallt wanneer de parsing faalt
-            if (process.env.NODE_ENV !== 'production') {
-              errorToChannel(msg.channel.id, msg.client, err);
-            } else if (process.env.ERROR_CHANNEL) {
-              msg.channel.send('Even normaal doen!');
-              errorToChannel(process.env.ERROR_CHANNEL, msg.client, err);
-            }
 
-            console.error(err);
-          }).finally(() => {
-            msg.channel.stopTyping(true);
-          });
+                return msg.channel.send(response, { split: true }).catch(() => { });
+              }
+
+              return null;
+            })
+            .finally(() => {
+              msg.channel.stopTyping(true);
+              return em.flush();
+            })
+            .catch((err) => {
+              // Dit wordt gecallt wanneer de parsing faalt
+              if (process.env.NODE_ENV !== 'production') {
+                errorToChannel(msg.channel.id, msg.client, err).catch(() => { console.log('Error could not be send :('); });
+              } else if (process.env.ERROR_CHANNEL) {
+                msg.channel.send('Even normaal doen!').catch(() => {});
+                errorToChannel(process.env.ERROR_CHANNEL, msg.client, err).catch(() => { console.log('Stel error kanaal in'); });
+              }
+
+              console.error(err);
+            });
         }
       }
     });
@@ -108,13 +115,13 @@ class EiNoah {
     this.router.onInit = this.onInit;
 
     this.router.initialize(this.client, orm);
-    process.on('uncaughtException', (err) => {
-      if (process.env.ERROR_CHANNEL) errorToChannel(process.env.ERROR_CHANNEL, this.client, err);
+    process.on('uncaughtException', async (err) => {
+      if (process.env.ERROR_CHANNEL) await errorToChannel(process.env.ERROR_CHANNEL, this.client, err, ErrorType.Uncaught);
     });
 
     process.on('unhandledRejection', (err) => {
       if (err instanceof Error && process.env.ERROR_CHANNEL) {
-        errorToChannel(process.env.ERROR_CHANNEL, this.client, err);
+        errorToChannel(process.env.ERROR_CHANNEL, this.client, err, ErrorType.Unhandled);
       }
     });
   }
