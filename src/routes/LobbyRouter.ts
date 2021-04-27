0cf89d2a0c85042923cbb0461b0a763ff970fd45
Ayrton-Taede Tromp <ayrton.taede@gmail.com>
2021-04-27 18:09:17 +0200

    Hotfix
    *Fixed:* Toegestaande members van een private lobby mochten de message history niet zien

--- src/routes/LobbyRouter.ts
+++ src/routes/LobbyRouter.ts
@@ -202,7 +202,7 @@ function getTextPermissionOverwrites(voice : VoiceChannel) : OverwriteData[] {
     }
 
     return {
-      allow: [Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.ADD_REACTIONS],
+      allow: [Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.ADD_REACTIONS, Permissions.FLAGS.READ_MESSAGE_HISTORY],
       id: overwrite.id,
     };
   });
