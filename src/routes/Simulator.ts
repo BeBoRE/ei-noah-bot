2eebc675be09a852bad91389d81944e8c82cdf4f
Ayrton-Taede Tromp <ayrton.taede@gmail.com>
2021-09-01 13:37:20 +0200

    Cache The Simulation
    *Veranderd:* Model wordt nu gecached

--- src/routes/Simulator.ts
+++ src/routes/Simulator.ts
@@ -7,52 +7,59 @@ import Router, { HandlerType } from '../router/Router';
 
 const router = new Router('Simuleer je vrienden');
 
+const userChain = new Map<string, Chain<string>>();
+
 router.use('user', async ({ flags, params, msg }) => {
   const [user] = flags.get('persoon') || params;
 
   if (!(user instanceof User)) return 'Geef iemand om te simuleren';
 
-  const spliced : string[] = new Array<string>().concat(...(await Promise.all(msg.guild.channels.cache
-    .filter((channel) : channel is BaseGuildTextChannel => {
-      const permissions = msg.client.user && channel.permissionsFor(msg.client.user);
-      return !!(channel.isText() && permissions?.has('READ_MESSAGE_HISTORY') && permissions.has('VIEW_CHANNEL'));
-    })
-    .map(async (channel) => {
-      const messages : string[] = [];
+  let chain : Chain<string> | undefined = userChain.get(user.id);
+  if (!chain) {
+    const spliced : string[] = new Array<string>().concat(...(await Promise.all(msg.guild.channels.cache
+      .filter((channel) : channel is BaseGuildTextChannel => {
+        const permissions = msg.client.user && channel.permissionsFor(msg.client.user);
+        return !!(channel.isText() && permissions?.has('READ_MESSAGE_HISTORY') && permissions.has('VIEW_CHANNEL'));
+      })
+      .map(async (channel) => {
+        const messages : string[] = [];
 
-      let lastMessage : string | undefined = channel.lastMessageId ?? undefined;
-      if (lastMessage) {
-        let iterations = 0;
-        while (lastMessage && iterations < 20 && messages.length < 150) {
-          iterations += 1;
-          // console.log(`---=== Fetching in ${channel.name} ${channel.parent ? `(${channel.parent.name})` : ''} ===---`);
-          // eslint-disable-next-line no-await-in-loop
-          const fetchedMessages = await channel.messages.fetch({ limit: 100, before: lastMessage })
-          // eslint-disable-next-line no-loop-func
-            .then((msgs) => {
-              lastMessage = msgs.last()?.id !== lastMessage ? msgs.last()?.id : undefined;
-              return msgs;
-            })
-          // eslint-disable-next-line no-loop-func
-            .catch((err) => console.log(err));
+        let lastMessage : string | undefined = channel.lastMessageId ?? undefined;
+        if (lastMessage) {
+          let iterations = 0;
+          while (lastMessage && iterations < 20 && messages.length < 150) {
+            iterations += 1;
+            // console.log(`---=== Fetching in ${channel.name} ${channel.parent ? `(${channel.parent.name})` : ''} ===---`);
+            // eslint-disable-next-line no-await-in-loop
+            const fetchedMessages = await channel.messages.fetch({ limit: 100, before: lastMessage })
+            // eslint-disable-next-line no-loop-func
+              .then((msgs) => {
+                lastMessage = msgs.last()?.id !== lastMessage ? msgs.last()?.id : undefined;
+                return msgs;
+              })
+            // eslint-disable-next-line no-loop-func
+              .catch((err) => console.log(err));
 
-          fetchedMessages?.forEach((m) => {
-            if (m.content !== '' && m.author.id === user.id) {
-              // console.log(m.content);
-              messages.push(m.content);
-            }
-          });
+            fetchedMessages?.forEach((m) => {
+              if (m.content !== '' && m.author.id === user.id) {
+                // console.log(m.content);
+                messages.push(m.content);
+              }
+            });
+          }
         }
-      }
 
-      // console.log(`---=== Completed ${channel.name} ${channel.parent ? `(${channel.parent.name})` : ''} ===---`);
-      return messages;
-    }))));
+        // console.log(`---=== Completed ${channel.name} ${channel.parent ? `(${channel.parent.name})` : ''} ===---`);
+        return messages;
+      }))));
 
-  if (spliced.length < 50) return 'Niet genoeg berichten gevonden om iets mee te genereren';
-  console.log(`${spliced.length} berichten gevonden`);
+    if (spliced.length < 50) return 'Niet genoeg berichten gevonden om iets mee te genereren';
+    console.log(`${spliced.length} berichten gevonden`);
 
-  const chain = new Chain(spliced.map((m) => m.split(' '), { stateSize: 3 }).slice(0, spliced.length > 500 ? 500 : -1));
+    chain = new Chain(spliced.map((m) => m.split(' '), { stateSize: 2 }).slice(0, spliced.length > 650 ? 650 : -1));
+    userChain.set(user.id, chain);
+  }
+
   const text = chain.walk().join(' ');
 
   const embed = new MessageEmbed();
