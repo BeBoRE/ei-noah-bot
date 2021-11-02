712abbd838501efe6b63371edd4262bb85986743
Ayrton-Taede Tromp <ayrton.taede@gmail.com>
2021-11-02 15:24:36 +0100

    CUMulatief xD
    *Toevoegd:* `/corona graph` kan nu grafieken genereren met de cumulatieve data van de dagen daarvoor
    
    *Veranderd:* `/corona remove` automatisch aanvullen geeft nu alleen de gemeentes die op dit moment in jouw verslag komen
    
    *Veranderd:* `/corona graph` kan nu ook doden en ziekenhuis opnames in de grafiek laten zien
    
    *Veranderd:* Corona data wordt nu om 4 uur opgehaald en als verslag verstuurd

--- src/routes/CoronaRouter.ts
+++ src/routes/CoronaRouter.ts
@@ -5,9 +5,10 @@ import { Client, MessageAttachment, Collection } from 'discord.js';
 import { MikroORM } from '@mikro-orm/core';
 import { PostgreSqlDriver, EntityManager } from '@mikro-orm/postgresql';
 import { ChartJSNodeCanvas } from 'chartjs-node-canvas';
-import { ChartConfiguration } from 'chart.js';
+import Chart, { ChartConfiguration } from 'chart.js';
 import { i18n as I18n } from 'i18next';
 import stringSimilarity from 'string-similarity';
+import { CronJob } from 'cron';
 import UserCoronaRegions from '../entity/UserCoronaRegions';
 import Router, { BothAutocompleteHandler, BothHandler, HandlerType } from '../router/Router';
 import CoronaData, { CoronaInfo } from '../entity/CoronaData';
@@ -117,6 +118,18 @@ const removeHandler : BothHandler = async ({
   return `${dbRegion.region} is verwijderd van je dagelijkse rapport`;
 };
 
+const removeAutocompleteHandler : BothAutocompleteHandler = async ({ flags, user }) => {
+  const [inputCommunity] = flags.get('region') || [];
+  if (typeof inputCommunity !== 'string') return [{ name: 'Not a string', value: 'notAString' }];
+
+  if (!user.coronaRegions.isInitialized()) await user.coronaRegions.init();
+  const collection = new Collection([...user.coronaRegions.getItems().entries()]);
+
+  return collection.sort((a, b) => stringSimilarity.compareTwoStrings(inputCommunity.toLowerCase(), b.region.toLowerCase()) - stringSimilarity.compareTwoStrings(inputCommunity.toLowerCase(), a.region.toLowerCase()))
+    .first(25)
+    .map((item) => ({ name: item.region, value: item.region }));
+};
+
 router.use('remove', removeHandler, HandlerType.BOTH, {
   description: 'Verwijder een regio/gemeente',
   options: [{
@@ -126,7 +139,7 @@ router.use('remove', removeHandler, HandlerType.BOTH, {
     autocomplete: true,
     required: true,
   }],
-}, communityAutocompleteHandler);
+}, removeAutocompleteHandler);
 router.use('verwijder', removeHandler);
 router.use('delete', removeHandler);
 
@@ -152,19 +165,26 @@ enum Niveau {
   ZeerErnstig = 'Zeer Ernstig'
 }
 
-interface CoronaRollingData {
+interface CoronaDataManipulated {
   date: Date,
   community: string,
-  total_reported_weekly: string
-  hospital_admissions_weekly: string,
-  deceased_weekly: string
+  total_reported: string
+  hospital_admissions: string,
+  deceased: string
 }
 
-const getRollingData = (em: EntityManager, communities : string[]) : Promise<CoronaRollingData[]> => {
+const getRollingData = (em: EntityManager, communities : string[]) : Promise<CoronaDataManipulated[]> => {
   const query = em.createQueryBuilder(CoronaData, 'cd', 'read')
-    .raw(`SELECT community, date, sum(total_reported) over(partition by community order by date rows between 6 preceding and current row) as total_reported_weekly, sum(deceased) over(partition by community order by date rows between 6 preceding and current row) as deceased_weekly, sum(hospital_admissions) over(partition by community order by date rows between 6 preceding and current row) as hospital_admissions_weekly from corona_data where lower(community) IN (${communities.map(() => '?').join(',')}) order by date`, ...[communities.map((c) => c.toLowerCase())]);
+    .raw(`SELECT community, date, sum(total_reported) over(partition by community order by date rows between 6 preceding and current row) as total_reported, sum(deceased) over(partition by community order by date rows between 6 preceding and current row) as deceased, sum(hospital_admissions) over(partition by community order by date rows between 6 preceding and current row) as hospital_admissions from corona_data where lower(community) IN (${communities.map(() => '?').join(',')}) order by date`, ...[communities.map((c) => c.toLowerCase())]);
 
-  return em.execute<CoronaRollingData[]>(query);
+  return em.execute<CoronaDataManipulated[]>(query);
+};
+
+const getCumulative = (em : EntityManager, communities : string[]) : Promise<CoronaDataManipulated[]> => {
+  const query = em.createQueryBuilder(CoronaData, 'cd', 'read')
+    .raw(`SELECT community, date, sum(total_reported) over(partition by community order by date) as total_reported, sum(deceased) over(partition by community order by date) as deceased, sum(hospital_admissions) over(partition by community order by date) as hospital_admissions from corona_data where lower(community) IN (${communities.map(() => '?').join(',')}) order by date`, ...[communities.map((c) => c.toLowerCase())]);
+
+  return em.execute<CoronaDataManipulated[]>(query);
 };
 
 const scale = 1;
@@ -179,20 +199,63 @@ canvas.registerFont('./fonts/Roboto-Black.ttf', {
   family: 'Roboto Black',
 });
 
-const generateGraph = (data : CoronaRollingData[], days = 7, displayLabels = false, i18n ?: I18n) => {
+interface GraphOptions {
+  data: CoronaDataManipulated[],
+  days: number,
+  displayLabels : boolean,
+  i18n ?: I18n
+  showCases ?: boolean,
+  showAdmissions ?: boolean,
+  showDeaths ?: boolean
+}
+
+const generateGraph = ({
+  data,
+  days = 7,
+  displayLabels = false,
+  i18n,
+  showCases = true, showAdmissions = false, showDeaths = false,
+} : GraphOptions) => {
   const collection = new Collection(data.entries());
+  const datasets : Chart.ChartDataSets[] = [];
+
+  if (showCases) {
+    datasets.push({
+      label: 'Gevallen',
+      data: collection.last(days).map((value) => Number.parseInt(value.total_reported, 10)),
+      backgroundColor: '#ffcc5f11',
+      borderColor: '#ffcc5f',
+      borderWidth: 3 * scale,
+    });
+  }
+
+  if (showAdmissions) {
+    datasets.push({
+      label: 'Ziekenhuis Opnames',
+      data: collection.last(days).map((value) => Number.parseInt(value.hospital_admissions, 10)),
+      backgroundColor: '#03fcfc11',
+      borderColor: '#03fcfc',
+      borderWidth: 3 * scale,
+    });
+  }
+
+  if (showDeaths) {
+    datasets.push({
+      label: 'Doden',
+      data: collection.last(days).map((value) => Number.parseInt(value.deceased, 10)),
+      backgroundColor: '#91090911',
+      borderColor: '#910909',
+      borderWidth: 3 * scale,
+    });
+  }
+
+  if (!datasets.length) throw Error('Can\'t generate a graph without data');
 
   const configuration : ChartConfiguration = {
     type: 'line',
     data: {
       labels: collection.last(days).map((value) => moment(value.date).locale(i18n?.language || 'nl').format(days < 90 ? 'DD' : 'DD MMMM')),
-      datasets: [{
-        label: collection.first()?.community,
-        data: collection.last(days).map((value) => Number.parseInt(value.total_reported_weekly, 10)),
-        backgroundColor: '#ffcc5f11',
-        borderColor: '#ffcc5f',
-        borderWidth: 3 * scale,
-      }],
+      datasets,
     },
     options: {
       title: {
@@ -247,17 +310,29 @@ const coronaGraph : BothHandler = async ({
   const [community] = flags.get('region') || params;
   const [days] = flags.get('days') || [30];
   const [labels] = flags.get('labels') || [true];
+  const [cumulative] = flags.get('cumulative') || [false];
+  const [showCases] = flags.get('show-cases') || [true];
+  const [showAdmissions] = flags.get('show-admissions') || [false];
+  const [showDeaths] = flags.get('show-deaths') || [false];
 
   if (typeof community !== 'string') return 'Community moet een gemeente zijn en niet een random string';
   if (typeof days !== 'number') return 'Last-days moet een nummer zijn';
-  if (typeof labels !== 'boolean') return 'Labels moet van type boolean zijn';
+  if (typeof labels !== 'boolean') return 'Labels moet van type boolean zijn (true/false)';
+  if (typeof cumulative !== 'boolean') return 'Cumulative moet van type boolean zijn (true/false)';
+  if (typeof showCases !== 'boolean') return 'Show-cases moet van type boolean zijn (true/false)';
+  if (typeof showAdmissions !== 'boolean') return 'Show-admissions moet van type boolean zijn (true/false)';
+  if (typeof showDeaths !== 'boolean') return 'Show-deaths moet van type boolean zijn (true/false)';
 
-  const data = await getRollingData(em, [community]);
+  if (!showCases && !showAdmissions && !showDeaths) return 'Er moet een dataset geselecteerd zijn';
+
+  const data = cumulative ? await getCumulative(em, [community]) : await getRollingData(em, [community]);
   // const last30days = allData.filter((d) => d.date.getTime() > moment().subtract(1, 'month').toDate().getTime());
 
   if (!data.length) return `${community} is niet een gemeente`;
 
-  return new MessageAttachment(await generateGraph(data, days, labels, i18n));
+  return new MessageAttachment(await generateGraph({
+    data, days, displayLabels: labels, i18n, showCases, showAdmissions, showDeaths,
+  }));
 };
 
 router.use('graph', coronaGraph, HandlerType.BOTH, {
@@ -276,6 +351,22 @@ router.use('graph', coronaGraph, HandlerType.BOTH, {
     name: 'labels',
     type: 'BOOLEAN',
     description: 'Laat de labels zien',
+  }, {
+    name: 'cumulative',
+    type: 'BOOLEAN',
+    description: 'Tel alle getallen van de datum terug op (i.p.v alleen de laatste 7 dagen)',
+  }, {
+    name: 'show-cases',
+    type: 'BOOLEAN',
+    description: 'Laat gevallen zien',
+  }, {
+    name: 'show-admissions',
+    type: 'BOOLEAN',
+    description: 'Laat ziekenhuis opnamens zien',
+  }, {
+    name: 'show-deaths',
+    type: 'BOOLEAN',
+    description: 'Laat gevallen zien',
   }],
 }, communityAutocompleteHandler);
 
@@ -292,7 +383,7 @@ const coronaRefresher = async (client : Client, orm : MikroORM<PostgreSqlDriver>
 
     if (allRollingData.length === 0) return;
 
-    const rollingDataPerRegion = new Map<string, CoronaRollingData[]>();
+    const rollingDataPerRegion = new Map<string, CoronaDataManipulated[]>();
     allRollingData.forEach((report) => {
       const currentArray = rollingDataPerRegion.get(report.community.toLowerCase());
       if (currentArray) {
@@ -315,13 +406,13 @@ const coronaRefresher = async (client : Client, orm : MikroORM<PostgreSqlDriver>
         const population = regionPopulations[r.region.toLowerCase()] || regionPopulations[`${r.region.toLowerCase()} (gemeente)`];
 
         if (!rollingData || !population) return `Er is iets fout gegaan bij het weergeven van ${r.region}`;
-        graphs.push(generateGraph(rollingData, 30, true).then((buffer) => new MessageAttachment(buffer, `${r.region}_graph_${moment(rollingData[0].date).format('DD-MM-YYYY')}.png`)));
+        graphs.push(generateGraph({ data: rollingData, days: 30, displayLabels: true }).then((buffer) => new MessageAttachment(buffer, `${r.region}_graph_${moment(rollingData[0].date).format('DD-MM-YYYY')}.png`)));
 
         const weeklyCount = rollingData[rollingData.length - 1];
 
-        const cases = Number.parseInt(weeklyCount.total_reported_weekly, 10);
-        const hospital = Number.parseInt(weeklyCount.hospital_admissions_weekly, 10);
-        const deceased = Number.parseInt(weeklyCount.deceased_weekly, 10);
+        const cases = Number.parseInt(weeklyCount.total_reported, 10);
+        const hospital = Number.parseInt(weeklyCount.hospital_admissions, 10);
+        const deceased = Number.parseInt(weeklyCount.deceased, 10);
 
         const casesPer = Math.ceil((cases / population) * 100_000);
         const hospitalPer = Math.ceil((hospital / population) * 100_000);
@@ -387,10 +478,11 @@ const coronaRefresher = async (client : Client, orm : MikroORM<PostgreSqlDriver>
       } else {
         console.log(`${moment().format('HH:mm')}: no new data`);
       }
-    } finally {
-      setTimeout(refreshData, 1000 * 60 * 60 * 3);
-    }
+    } catch (err) { console.log(err); }
   };
+
+  const refreshDataCron = new CronJob('0 16 * * *', refreshData, null, false, 'Europe/Amsterdam');
+  refreshDataCron.start();
 
   if (process.env.REFRESH_DATA?.toLowerCase() !== 'false') {
     refreshData();
