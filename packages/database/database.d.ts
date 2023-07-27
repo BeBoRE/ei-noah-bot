f48047a440e39e98372c5e9238aec1a0bd1bc289
Ayrton-Taede Tromp <ayrton@bebore.com>
2023-07-27 15:44:49 +0200

    Fixed tserror for real

--- packages/database/database.d.ts
+++
@@ -1,7 +0,0 @@
-type MikroORM = import('@mikro-orm/postgresql').MikroORM;
-
-declare module NodeJS {
-  interface Global {
-    __MikroORM__: MikroORM;
-  }
-}
--- packages/database/index.ts
+++ packages/database/index.ts
@@ -4,6 +4,15 @@ import {PostgreSqlDriver, MikroORM} from '@mikro-orm/postgresql';
 
 export default options;
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface Global {
+      __MikroORM__: PostgreSqlMikroORM | undefined;
+    }
+  }
+}
+
 const getOrm = async () : Promise<PostgreSqlMikroORM> => {
   if(!global.__MikroORM__) {
     global.__MikroORM__ = await MikroORM.init<PostgreSqlDriver>(options).catch((err) => { console.error(err); process.exit(-1); })
@@ -12,4 +21,4 @@ const getOrm = async () : Promise<PostgreSqlMikroORM> => {
   return global.__MikroORM__;
 }
 
-export {getOrm};
+export { getOrm };
