1c7231c2e8571d28518b20e47cb7d491776ecb7f
Ayrton-Taede Tromp <ayrton@bebore.com>
2023-08-01 21:37:15 +0200

    Now display's guild's icon

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -2,16 +2,29 @@ import { SafeAreaView } from "react-native-safe-area-context"
 import { api } from "../utils/api"
 import Text from "src/components/Text";
 import { Stack } from "expo-router";
-import { ActivityIndicator, View } from "react-native";
-import Image from "next/image";
+import { ActivityIndicator, Pressable, View } from "react-native";
+import { Image } from "expo-image";
 
 const Screen = () => {
-  const {data, isLoading} = api.lobby.all.useQuery();
+  const {data, isLoading, error, isRefetching, refetch} = api.lobby.all.useQuery();
 
   if(isLoading) return (
     <View className="flex-1 align-middle justify-center">
       <ActivityIndicator size={"large"}></ActivityIndicator>
     </View>
+  )
+
+  if(!data && error) return (
+    <View className="flex-1 align-middle justify-center">
+      <Text className="text-center text-3xl">Error loading lobbies</Text>
+    </View>
+  )
+
+  if(data && data.length === 0) return (
+    <Pressable className="flex-1 align-middle justify-center" onPress={() => {refetch()}}>
+      <Text className="text-center text-3xl m-3">Please join a lobby</Text>
+      <ActivityIndicator className={isRefetching ? 'opacity-100' : 'opacity-0 transition-opacity'} size={"large"}></ActivityIndicator>
+    </Pressable>
   )
 
   const guild = data && data[0] && data[0].guild.success ? data[0].guild.data : null;
@@ -22,10 +35,13 @@ const Screen = () => {
     </View>
   )
 
+  const style = "w-32 h-32 bg-primary-900 rounded-full ring-4 outline-primary";
+
   return (
     <View className="p-5">
-      <View>
-        {guild.icon ? <Image src={guild.icon} className="h-8 w-8" alt=""/> : <View className="h-8 w-8 bg-secondary"/>}
+      <View className="items-center">
+        {guild.icon ? <Image source={guild.icon} className={[style, 'ring-4'].join(' ')} alt=""/> : <View className={`${style} bg-secondary`}/>}
+        <Text className="text-center text-2xl p-3 font-bold">{guild.name}</Text>
       </View>
     </View>
   )
