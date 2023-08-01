51a411589bbb1996b616fbc1bf73dca525b786e9
Ayrton-Taede Tromp <ayrton@bebore.com>
2023-08-01 16:24:00 +0200

    Added some colors to config

--- packages/tailwind-config/index.ts
+++ packages/tailwind-config/index.ts
@@ -6,7 +6,19 @@ const baseConfig = {
     colors: {
       text: '#fbefd5',
       background: '#0e0a01',
-      primary: '#ffcc5f',
+      primary: {
+        DEFAULT: '#ffcc5f',
+        50: '#fff7e5',
+        100: '#ffe7b3',
+        200: '#ffd680',
+        300: '#ffc64d',
+        400: '#ffb61a',
+        500: '#e69c00',
+        600: '#b37a00',
+        700: '#805700',
+        800: '#4d3400',
+        900: '#1a1100'
+    },
       secondary: '#382806',
       accent: '#e9a920',
     },
