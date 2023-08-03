bd0af77e21ac2937be4ccee6f505c636bd5f5746
Ayrton-Taede Tromp <ayrton@bebore.com>
2023-08-03 17:43:51 +0200

    Removed pusher client package

--- packages/pusher-client/index.ts
+++
@@ -1,9 +0,0 @@
-import Pusher from 'pusher'
-
-export const pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID || '',
-  key: process.env.PUSHER_KEY || '',
-  secret: process.env.PUSHER_SECRET || '',
-  cluster: process.env.PUSHER_CLUSTER || '',
-  useTLS: true
-});
