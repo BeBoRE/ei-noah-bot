05c82452666017c3fb09353aef19e2d0491a364b
Ayrton-Taede Tromp <ayrton@bebore.com>
2023-08-04 23:19:20 +0200

    Changed something

--- apps/mobile/src/components/JoinLobby.tsx
+++ apps/mobile/src/components/JoinLobby.tsx
@@ -1,14 +1,11 @@
 import { View } from "react-native";
-import { api } from "src/utils/api";
 import Text from "./Text";
 import { Image } from "expo-image"
 
 const JoinLobby = () => {
-  const {data: user} = api.user.me.useQuery();
-
   return (
     <View className="flex-1 justify-center">
-      <Text className="text-center text-3xl m-3">Please join a lobby {user?.globalName}</Text>
+      <Text className="text-center text-3xl m-3">Please join a lobby</Text>
       <View className="px-5">
         <View className="rounded-md bg-[#2b2d31]">
           <View className="flex-row items-center p-3">
