10c3bcb8e5cb9a71c35d3228bb2e1b7a01a45989
Ayrton-Taede Tromp <ayrton@bebore.com>
2023-08-05 12:27:02 +0200

    Fixed security for website

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -1,14 +1,9 @@
 import { Suspense } from "react";
-import { Users } from "./user";
-import { Lobbies } from "./lobby";
 
 export default function Page() {
   return (
-    <>
-      <Suspense fallback={<span>...Loading</span>}>
-        <Users/>
-        <Lobbies></Lobbies>
-      </Suspense>
-    </>
+    <h1>
+      Er is hier niks. Ga weg.
+    </h1>
   );
 }
--- packages/trpc/src/routes/users.ts
+++ packages/trpc/src/routes/users.ts
@@ -1,11 +1,6 @@
-import {User} from '@ei/database/entity/User'
-
-import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
-  all: publicProcedure.query(async ({ ctx }) => {
-    return await ctx.em.getRepository(User).findAll();
-  }),
   me: protectedProcedure.query(async ({ ctx }) => {
     return ctx.session?.user;
   })
