56f029d8fb956dd606d0a18922439376560c48e1
Ayrton-Taede Tromp <ayrton.taede@gmail.com>
2023-08-07 14:48:24 +0200

    Now using correct url

--- apps/mobile/src/config.ts
+++ apps/mobile/src/config.ts
@@ -1,14 +1,39 @@
-const config = {
+let developmentConfig : Config | null = null;
+try {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  developmentConfig = require("config.development").default;
+} catch (e) {
+  developmentConfig = null;
+}
+
+type Config = {
+  pusher: {
+    appId: string;
+    appKey: string;
+    cluster: string;
+  };
+  api: {
+    url: string;
+  };
+  discord: {
+    clientId: string;
+  };
+}
+
+const config : Config = {
   pusher: {
     appId: process.env.EXPO_PUBLIC_PUSHER_APP_ID || "1645825",
     appKey: process.env.EXPO_PUBLIC_PUSHER_KEY || "e53b6fdba8cac687678b",
     cluster: process.env.EXPO_PUBLIC_PUSHER_CLUSTER || "eu",
+    ...developmentConfig?.pusher
   }, 
   api: {
-    url: process.env.EXPO_PUBLIC_VERCEL_URL,
+    url: process.env.EXPO_PUBLIC_VERCEL_URL || "https://ei.sweaties.net",
+    ...developmentConfig?.api
   },
   discord: {
     clientId: process.env.EXPO_PUBLIC_CLIENT_ID || "730913870805336195",
+    ...developmentConfig?.discord
   }
 }
 
--- apps/mobile/src/utils/api.tsx
+++ apps/mobile/src/utils/api.tsx
@@ -12,6 +12,7 @@ import { useAuth } from "src/context/auth";
 import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import config from "src/config";
 
 onlineManager.setEventListener(setOnline => {
   return NetInfo.addEventListener(state => {
@@ -36,27 +37,7 @@ export { type RouterInputs, type RouterOutputs } from "@ei/trpc";
  * setting the baseUrl to your production API URL.
  */
 const getBaseUrl = () => {
-  /**
-   * Gets the IP address of your host-machine. If it cannot automatically find it,
-   * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
-   * you don't have anything else running on it, or you'd have to change it.
-   *
-   * **NOTE**: This is only for development. In production, you'll want to set the
-   * baseUrl to your production API URL.
-   */
-
-  const debuggerHost = Constants.expoConfig?.hostUri;
-  const localhost = debuggerHost?.split(":")[0];
-
-  if (process.env.EXPO_PUBLIC_VERCEL_URL)
-      return process.env.EXPO_PUBLIC_VERCEL_URL;
-
-  if (!localhost) {
-    throw new Error(
-      "Failed to get localhost. Please define your api url with EXPO_PUBLIC_VERCEL_URL.",
-    );
-  }
-  return `http://${localhost}:3000`;
+  return config.api.url;
 };
 
 const asyncStoragePersistor = createAsyncStoragePersister({
