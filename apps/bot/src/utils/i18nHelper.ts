562f28316bb7392b786e6e899101ad12d84135aa
Ayrton-Taede Tromp <ayrton@bebore.com>
2023-08-08 14:12:30 +0200

    Removed unused function

--- apps/bot/src/utils/i18nHelper.ts
+++ apps/bot/src/utils/i18nHelper.ts
@@ -4,10 +4,6 @@ import { type i18n } from "i18next";
 
 const defaultLanguage = "nl";
 
-export const createI18nClone = (i18n : i18n, locale: string) => {
-  const newI18n = i18n.cloneInstance({ lng: locale });
-}
-
 export const getLocale = ({user, guild} : { user?: User, guild?: Guild}) => {
   // Prioritize user locale
   if (user && user.language) {
