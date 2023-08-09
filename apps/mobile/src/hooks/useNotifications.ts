7e495eab95aba52a5ef637602f946daa2682b03d
Ayrton-Taede Tromp <ayrton@bebore.com>
2023-08-09 13:44:13 +0200

    Fixing push notification not being handled

--- apps/mobile/src/hooks/useNotifications.ts
+++ apps/mobile/src/hooks/useNotifications.ts
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 import { useAuth } from "src/context/auth"
 import { api, createVanillaApi } from "src/utils/api"
 import registerForPushNotificationsAsync from "src/utils/registerForPushNotifications"
-import { addNotificationResponseReceivedListener } from 'expo-notifications'
+import { useLastNotificationResponse } from 'expo-notifications'
 import { PusherTasks } from "src/context/pusher"
 import { userAddNotificationSchema, userIdToPusherChannel } from "@ei/lobby"
 import { setNotificationCategoryAsync } from "expo-notifications"
@@ -42,37 +42,38 @@ const useNotifications = () => {
     }).catch(error => {
       console.log(error)
     })
-
-    try {
-      addNotificationResponseReceivedListener(async (response) => {
-        if ('categoryIdentifier' in response.notification.request.content && response.notification.request.content.categoryIdentifier === 'userAdd' && response.actionIdentifier === 'accept' && me) {
-          const data = userAddNotificationSchema.safeParse(response.notification.request.content.data);
-  
-          if (!data.success) return;
-          
-          const authInfo = await secureStorage.get('discordOauth')
-          if (!authInfo) return;
-  
-          const client = createVanillaApi(authInfo.accessToken)
-  
-          const outsidePusher = PusherTasks(client);
-
-          const user = await client.user.me.query()
-  
-          outsidePusher.signin()
-          outsidePusher.bind('pusher:signin_success', () => {
-            outsidePusher.subscribe(userIdToPusherChannel(user)).bind('pusher:subscription_succeeded', () => {
-              outsidePusher?.send_event('client-add-user', {
-                user: {id: data.data.userId},
-              }, userIdToPusherChannel(user))
-            })
-          })
-        }
-      })
-    } catch (error) {
-      console.log(error)
-    }
   }, [authInfo?.accessToken])
+
+  const response = useLastNotificationResponse();
+  useEffect(() => {
+    (async () => {
+      if (!response) return;
+
+      if ('categoryIdentifier' in response.notification.request.content && response.notification.request.content.categoryIdentifier === 'userAdd' && response.actionIdentifier === 'accept' && me) {
+        const data = userAddNotificationSchema.safeParse(response.notification.request.content.data);
+
+        if (!data.success) return;
+        
+        const authInfo = await secureStorage.get('discordOauth')
+        if (!authInfo) return;
+
+        const client = createVanillaApi(authInfo.accessToken)
+
+        const outsidePusher = PusherTasks(client);
+
+        const user = await client.user.me.query()
+
+        outsidePusher.signin()
+        outsidePusher.bind('pusher:signin_success', () => {
+          outsidePusher.subscribe(userIdToPusherChannel(user)).bind('pusher:subscription_succeeded', () => {
+            outsidePusher?.send_event('client-add-user', {
+              user: {id: data.data.userId},
+            }, userIdToPusherChannel(user))
+          })
+        })
+      }
+    })()
+  }, [response])
 }
 
 export default useNotifications;
