c51ce12ccb6f388807d28a4460ebc3b718beafb6
Ayrton-Taede Tromp <ayrton@bebore.com>
2023-10-12 16:12:52 +0200

    Moved generateURL token to lucia package

---
+++ packages/lucia/token.ts
@@ -0,0 +1,49 @@
+import { and, eq, gt } from 'drizzle-orm';
+import { nanoid } from 'nanoid';
+
+import { getDrizzleClient } from '@ei/drizzle';
+import { loginTokens } from '@ei/drizzle/tables/schema';
+
+import { getHost } from './utils';
+
+const tokenUrl = (token: string, redirectUri?: string) => {
+  const redirect = redirectUri
+    ? `?redirect=${encodeURIComponent(redirectUri)}`
+    : '';
+
+  return `${getHost()}/login/token/${token}${redirect}`;
+};
+
+const generateLoginToken = () => nanoid(72);
+export const generateLoginURL = async (
+  userId: string,
+  redirectUri?: string,
+) => {
+  const drizzle = await getDrizzleClient();
+
+  const now = Date.now();
+
+  const [existingToken] = await drizzle
+    .select()
+    .from(loginTokens)
+    .where(and(eq(loginTokens.userId, userId), gt(loginTokens.expires, now)));
+
+  if (existingToken) {
+    return tokenUrl(existingToken.token, redirectUri);
+  }
+
+  const [newToken] = await drizzle
+    .insert(loginTokens)
+    .values({
+      token: generateLoginToken(),
+      userId,
+      expires: now + 1000 * 60 * 30,
+    })
+    .returning();
+
+  if (!newToken) {
+    throw new Error('Failed to create login token');
+  }
+
+  return tokenUrl(newToken.token, redirectUri);
+};
---
+++ packages/lucia/utils.ts
@@ -0,0 +1,7 @@
+import ip from 'ip';
+
+// Get's the hosts ip when in development mode
+export const getHost = () =>
+  process.env.NODE_ENV === 'production'
+    ? 'https://ei.sweaties.net'
+    : `http://${ip.address(undefined, 'ipv4')}:3000`;
