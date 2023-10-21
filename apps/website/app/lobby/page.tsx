a8feebc3bfa913bafa9908835673e42637903fe9
Ayrton-Taede Tromp <ayrton@bebore.com>
2023-10-21 21:14:59 +0200

    You can now change lobby type and user limit on website

--- apps/website/app/lobby/page.tsx
+++ apps/website/app/lobby/page.tsx
@@ -2,11 +2,19 @@
 
 import { Button } from 'app/_components/ui/button';
 
-import { generateLobbyName } from '@ei/lobby';
+import { ChannelType, generateLobbyName } from '@ei/lobby';
 import { useLobby } from '@ei/react-shared/context/lobby';
 
+import { AnimatePresence, motion } from 'framer-motion';
+
+const lobbyTypeEmoji = {
+  [ChannelType.Public]: '🔊',
+  [ChannelType.Mute]: '🙊',
+  [ChannelType.Nojoin]: '🔒',
+};
+
 function LobbyScreen() {
-  const { lobby } = useLobby();
+  const { lobby, changeChannelType, changeUserLimit } = useLobby();
 
   if (!lobby)
     return (
@@ -30,11 +38,13 @@ function LobbyScreen() {
     lobby.channel.name,
   );
 
-  return (
+  const limits = new Set([0, 2, 5, 10, lobby.channel.limit || 0]);
+
+  return (  
     <div className="flex flex-1 justify-center">
       <div className="container flex justify-center">
         {lobby && (
-          <div className="flex w-full max-w-lg flex-col gap-4 p-4">
+          <div className="flex w-full max-w-lg flex-col gap-3 p-4">
             <div className="flex flex-row items-center justify-center gap-4">
               <div>
                 {lobby.guild.icon ? (
@@ -52,18 +62,67 @@ function LobbyScreen() {
             <div className="flex h-16 rounded-full bg-primary-900 p-2 text-center text-2xl font-bold">
               <Button
                 variant="secondary"
-                className="aspect-square h-full rounded-full text-2xl"
+                className="aspect-square h-full rounded-full text-2xl relative overflow-hidden"
               >
-                {name?.icon}
+                <AnimatePresence initial={false}>
+                  <motion.span
+                    key={name?.icon}
+                    className='absolute inset-0 flex items-center justify-center'
+                    initial={{ y: -50, opacity: 1 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    exit={{ y: 50, opacity: 1 }}
+                  >
+                    {name?.icon}
+                  </motion.span>
+                </AnimatePresence>
               </Button>
               <div className="flex flex-1 items-center justify-center">
                 <span>{name?.name}</span>
               </div>
             </div>
+            <div className="flex h-16 rounded-full bg-primary-900 p-2 text-center text-2xl font-bold justify-center gap-8">
+              {[ChannelType.Public, ChannelType.Mute, ChannelType.Nojoin].map(
+                (type) => (
+                  <Button
+                    key={type}
+                    variant="ghost"
+                    className="aspect-square h-full rounded-full text-2xl relative hover:bg-primary-800/20"
+                    onClick={() => changeChannelType(type)}
+                  >
+                    {lobby.channel.type === type && (
+                      <motion.div layoutId='type-selected' className="absolute inset-0 rounded-full bg-primary-800" />
+                    )}
+                    <span className="absolute inset-0 flex items-center justify-center">
+                     {lobbyTypeEmoji[type]}
+                    </span>
+                  </Button>
+                ),
+              )}
+            </div>
+            <div className="flex h-16 rounded-full bg-primary-900 p-2 text-center text-2xl font-bold justify-center gap-8">
+              {Array.from(limits)
+                .sort((a, b) => a - b)
+                .map((limit) => (
+                  <Button
+                    key={limit}
+                    variant="outline"
+                    className="aspect-square h-full rounded-full text-2xl relative hover:bg-primary-800/20"
+                    onClick={() => changeUserLimit(limit)}
+                  >
+                    {lobby.channel.limit === limit && (
+                      <motion.div layoutId='user-limit-selected' className="absolute inset-0 rounded-full bg-primary-800" />
+                    )}
+                    <span className="absolute inset-0 flex items-center justify-center">
+                      {limit === 0 ? '∞' : limit}
+                    </span>
+                  </Button>
+                ))}
+              </div>
           </div>
         )}
       </div>
     </div>
+    
   );
 }
 
