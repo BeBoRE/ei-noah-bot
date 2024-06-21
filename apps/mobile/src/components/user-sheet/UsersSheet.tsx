c61eace12056ed6a7e3e8f259bc254633987f7af
Ayrton-Taede Tromp <ayrton@bebore.com>
2024-06-21 23:09:39 +0200

    Made bottomsheet a scrollable

--- apps/mobile/src/components/user-sheet/UsersSheet.tsx
+++ apps/mobile/src/components/user-sheet/UsersSheet.tsx
@@ -1,14 +1,24 @@
-import { View } from 'react-native';
-import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
-
-import { LobbyUser, RecentlyAddedUser } from '@ei/lobby';
-import baseConfig from '@ei/tailwind-config';
-
+import { Pressable, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { Image } from 'expo-image';
+import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet';
+
+import { LobbyUser, RecentlyAddedUser } from '@ei/lobby';
+import { api } from '@ei/react-shared';
+import baseConfig from '@ei/tailwind-config';
+
+import { Text } from '../ui/text';
 import UserItem from './UserItem';
 
-function UsersSheet({ users, recentlyAddedUsers }: { users: LobbyUser[], recentlyAddedUsers: RecentlyAddedUser[] }) {
+function UsersSheet({
+  users,
+  recentlyAddedUsers,
+}: {
+  users: LobbyUser[];
+  recentlyAddedUsers: RecentlyAddedUser[];
+}) {
+  const { mutate: addUser } = api.lobby.addUser.useMutation();
+
   return (
     <BottomSheet
       backgroundStyle={{
@@ -20,14 +30,33 @@ function UsersSheet({ users, recentlyAddedUsers }: { users: LobbyUser[], recentl
         backgroundColor: baseConfig.theme.colors.primary[700],
       }}
     >
-      <BottomSheetView>
-        <FlatList data={recentlyAddedUsers} renderItem={({ item }) => <Image source={item.avatar} />} horizontal />
-        <View className="p-5">
-          {users.map((user) => (
-            <UserItem key={user.id} user={user} />
-          ))}
+      <BottomSheetScrollView>
+        <View className="p-5 pb-0">
+          <Text className="pb-2 text-lg font-bold text-primary-50">
+            Quick Add
+          </Text>
+          <FlatList
+            horizontal
+            data={recentlyAddedUsers.length ? recentlyAddedUsers : [null]}
+            renderItem={({ item }) => (
+              item !== null ?
+              <Pressable onPress={() => addUser({ user: item })}>
+                <Image
+                  className="h-16 w-16 rounded-full"
+                  source={item.avatar}
+                />
+              </Pressable> : <View className="h-16 w-16 bg-primary-800 rounded-full" />
+            )}
+          />
         </View>
-      </BottomSheetView>
+        <View className="p-5">
+          {
+            users.map((user) => (
+              <UserItem key={user.id} user={user} />
+            ))
+          }
+        </View>
+      </BottomSheetScrollView>
     </BottomSheet>
   );
 }
